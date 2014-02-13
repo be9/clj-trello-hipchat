644dd124d94a268fdc9d279a93fdf90140a079eb
oleg dashevskii <olegdashevskii@gmail.com>
2014-02-13 19:37:47 +0700

    Import blank app template

---
+++ test/clj_trello_hipchat/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-trello-hipchat.core-test
+  (:require [clojure.test :refer :all]
+            [clj-trello-hipchat.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
